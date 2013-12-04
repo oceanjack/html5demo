2ff18fdd8fb8a6629b90bb74a4fef019292729c6
oceanjack <1006840447@njust.edu.cn>
2013-12-04 09:58:15 +0800

    Update blur.js

--- blur.js
+++ blur.js
@@ -1,14 +1,13 @@
-//检测范围
 function inRange(i, width, height)
 {
 	return ((i >= 0) && (i < width * height * 4));
 }
-//模糊,每个像素按邻居取平均
+
 function averageNeighbors(imageData, width, height, i)
 {
-	//原本
+
 	var v = imageData[i];
-	//八个邻居
+
 	var north = inRange(i-width*4, width, height) ? imageData[i-width*4] : v;
 	var south = inRange(i+width*4, width, height) ? imageData[i+width*4] : v;
 	var west = inRange(i-4, width, height) ? imageData[i-4] : v;
@@ -17,7 +16,7 @@ function averageNeighbors(imageData, width, height, i)
 	var nw = inRange(i-width*4-4, width, height) ? imageData[i-width*4-4] : v;
 	var se = inRange(i+width*4+4, width, height) ? imageData[i+width*4+4] : v;
 	var sw = inRange(i+width*4-4, width, height) ? imageData[i+width*4-4] : v;
-	//取平均
+
 	var newVal = Math.floor((north + south + east + west + se + sw + ne + nw + v) / 9);
 	if(isNaN(newVal))
 	{
@@ -26,7 +25,7 @@ function averageNeighbors(imageData, width, height, i)
 	}
 	return newVal;
 }
-//模糊主函数
+
 function boxBlur(imageData, width, height)
 {
 	var data = [];
